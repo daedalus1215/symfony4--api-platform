7e6788a8932f44146adc6b74b1aaa67ed76a27cc
larry <ladams1776@yahoo.com>
2019-02-17 18:22:03 -0500

    delete works

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -13,6 +13,7 @@ use App\Entity\BlogPost;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Serializer;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
@@ -26,7 +27,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 class BlogController extends AbstractController
 {
     /**
-     * @Route("/{page}", name="blog_list", defaults={"page":5}, requirements={"page"="\d+"})
+     * @Route("/{page}", name="blog_list", defaults={"page":5}, requirements={"page"="\d+"}, methods={"GET"})
      */
     public function list($page = 1, Request $request)
     {
@@ -48,7 +49,7 @@ class BlogController extends AbstractController
     }
 
     /**
-     * @Route("/post/{id}", name="blog_by_id", requirements={"id"="\d+"})
+     * @Route("/post/{id}", name="blog_by_id", requirements={"id"="\d+"}, methods={"GET"})
      * @ParamConverter("post", class="App:BlogPost")
      */
     public function post(BlogPost $post)
@@ -57,7 +58,7 @@ class BlogController extends AbstractController
     }
 
     /**
-     * @Route("/post/{slug}", name="blog_by_slug")
+     * @Route("/post/{slug}", name="blog_by_slug", methods={"GET"})
      * @ParamConverter("post", options={"mapping": {"slug": "slug"}})
      */
     public function postBySlug(BlogPost $post)
@@ -87,5 +88,18 @@ class BlogController extends AbstractController
         $em->flush();
 
         return $this->json($blogPost);
+    }
+
+    /**
+     * @Route("/post/{id}", name="blog_delete", methods={"DELETE"})
+     * @ParamConverter("post", class="App:BlogPost")
+     */
+    public function delete(BlogPost $post)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->remove($post);
+        $entityManager->flush();
+
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
     }
 }
