0aea2c9cc4366f6e46fdad30a582f5fd0d95bde0
larry <ladams1776@yahoo.com>
2019-02-17 17:42:16 -0500

    Specifying the mapping with the postBySlug in this one.

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -58,6 +58,7 @@ class BlogController extends AbstractController
 
     /**
      * @Route("/post/{slug}", name="blog_by_slug")
+     * @ParamConverter("post", options={"mapping", {"slug", "slug"}})
      */
     public function postBySlug(BlogPost $post)
     {
